d08f33b45e2fc96ecdbb3e855e8b8abca5d4540f
Ravza Cetinkaya <143969992+ravza-ctk@users.noreply.github.com>
2026-02-20 15:40:37 +0300

    Add files via upload

---
+++ Flutter (frontend)/my_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\VICTUS\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\VICTUS\\flutter_project\\my_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\VICTUS\\flutter"
+  "PROJECT_DIR=C:\\Users\\VICTUS\\flutter_project\\my_app"
+  "FLUTTER_ROOT=C:\\Users\\VICTUS\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\VICTUS\\flutter_project\\my_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\VICTUS\\flutter_project\\my_app"
+  "FLUTTER_TARGET=C:\\Users\\VICTUS\\flutter_project\\my_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjZkZDkzZjlhMg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YTVjYjk2MzY5ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\VICTUS\\flutter_project\\my_app\\.dart_tool\\package_config.json"
+)
